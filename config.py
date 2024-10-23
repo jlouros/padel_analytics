84cdc0f36c4f9f29bb8201a93ae47c93560b3c3d
Alex Monras <amb.physis@gmail.com>
2024-10-23 10:43:42 +0200

    report mps if on mac

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ COLLECT_DATA = True
 COLLECT_DATA_PATH = "data.csv"
 
 # Maximum number of frames to be analysed
-MAX_FRAMES = 30
+MAX_FRAMES = None
 
 # Fixed court keypoints´
 FIXED_COURT_KEYPOINTS_LOAD_PATH = "./cache/fixed_keypoints_detection.json"
